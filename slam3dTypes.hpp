7f5f2f7a88af78a455edc6ae05118ff51eff9b6e
Sebastian Kasperski <sebastian.kasperski@dfki.de>
2014-09-15 12:00:02 +0200

    initial commit by rock-create-orogen

---
+++ slam3dTypes.hpp
@@ -0,0 +1,15 @@
+#ifndef slam3d_TYPES_HPP
+#define slam3d_TYPES_HPP
+
+/* If you need to define types specific to your oroGen components, define them
+ * here. Required headers must be included explicitly
+ *
+ * However, it is common that you will only import types from your library, in
+ * which case you do not need this file
+ */
+
+namespace slam3d {
+}
+
+#endif
+
