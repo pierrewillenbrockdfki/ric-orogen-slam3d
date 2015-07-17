180eaaa56e3d94e8ca58d9883f2045df65c6f24a
Sebastian Kasperski <sebastian.kasperski@dfki.de>
2015-07-17 15:43:02 +0200

    Check if odometry data has been received.

--- tasks/rock-common.hpp
+++ tasks/rock-common.hpp
@@ -7,6 +7,17 @@
 
 namespace slam3d
 {
+	long timevaldiff(const timeval& start, const timeval& end)
+	{
+		long msec;
+		msec=(end.tv_sec - start.tv_sec)*1000;
+		msec+=(end.tv_usec - start.tv_usec)/1000;
+		if(msec > 0)
+			return msec;
+		else
+			return -msec;
+	}
+	
 	class BaseLogger : public slam::Logger
 	{
 	public:
@@ -39,13 +50,22 @@ namespace slam3d
 	class RockOdometry : public slam::Odometry
 	{
 	public:
-		RockOdometry(slam::Logger* logger) : slam::Odometry(logger) {}
+		RockOdometry(slam::Logger* logger) : slam::Odometry(logger)
+		{
+			mCurrentPose = slam::Transform::Identity();
+			mTolerance = 100; // milliseconds
+
+		}
 		~RockOdometry() {}
 		
 		// Rock transformer cannot be queried for a specific timestamp.
 		// So we have to rely on current pose to match last received measurement.
 		slam::Transform getOdometricPose(timeval stamp)
 		{
+			if(timevaldiff(stamp, mCurrentTime) > mTolerance)
+			{
+				throw slam::OdometryException();
+			}
 			return mCurrentPose;
 		}
 		
@@ -64,6 +84,7 @@ namespace slam3d
 			{
 				mCurrentPose.linear() = odom_aff.linear();
 				mCurrentPose.translation() = odom_aff.translation();
+				mCurrentTime = pose.time.toTimeval();
 			}else
 			{
 				mLogger->message(slam::ERROR, "Odometry sample contained invalid data!");
@@ -73,5 +94,7 @@ namespace slam3d
 		
 	private:
 		slam::Transform mCurrentPose;
+		timeval mCurrentTime;
+		long mTolerance;
 	};
 }
