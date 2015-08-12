3dcd94ea61e834199ae84202df4ceb403200ab04
Sebastian Kasperski <sebastian.kasperski@dfki.de>
2015-08-12 16:41:54 +0200

    Updated asguard script.

--- scripts/map_asguard.rb
+++ scripts/map_asguard.rb
@@ -40,8 +40,8 @@ Orocos.run 'slam3d::PointcloudFilter' => 'filter',
 	projector = Orocos.name_service.get 'projector'
 	projector.size_x = 50
 	projector.size_y = 50
-	projector.offset_x = -10
-	projector.offset_y = -40
+	projector.offset_x = -25
+	projector.offset_y = -25
 	projector.min_z = -5;
 	projector.max_z = 5;
 	projector.resolution = 0.05
@@ -61,8 +61,8 @@ Orocos.run 'slam3d::PointcloudFilter' => 'filter',
 	mapper.odometry_time_tolerance = 500
 	mapper.log_level = 1
 	mapper.gicp_config do |c|
-		c.max_correspondence_distance = 1.0
-		c.max_fitness_score = 50
+		c.max_correspondence_distance = 3.0
+		c.max_fitness_score = 5
 	end
 	mapper.transformer_max_latency = 10
 	mapper.scan_period = 2
