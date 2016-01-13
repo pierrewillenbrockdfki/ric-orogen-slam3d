9fe5f8a8a69fc5469b80378ba0a4adffe1f064ad
Sebastian Kasperski <sebastian.kasperski@dfki.de>
2016-01-13 16:36:24 +0100

    Grid-Projection working.

--- tasks/PointcloudMapper2D.cpp
+++ tasks/PointcloudMapper2D.cpp
@@ -3,7 +3,29 @@
 #include <envire/Core.hpp>
 #include <envire/Orocos.hpp>
 
+#include <pcl/common/transforms.h>
+
 using namespace slam3d;
+
+// https://de.wikipedia.org/wiki/Bresenham-Algorithmus#Kompakte_Variante
+void line(int x0, int y0, int x1, int y1, int* array, int sizex, int sizey)
+{
+	int dx =  abs(x1-x0), sx = x0<x1 ? 1 : -1;
+	int dy = -abs(y1-y0), sy = y0<y1 ? 1 : -1;
+	int err = dx+dy, e2; /* error value e_xy */
+
+	while(1)
+	{
+		if(x0 >= 0 && x0 < sizex && y0 >= 0 && y0 < sizey)
+		{
+			array[(y0 * sizex) + x0] += 1;
+		}
+		if (x0==x1 && y0==y1) break;
+		e2 = 2*err;
+		if (e2 > dy) { err += dy; x0 += sx; } /* e_xy+e_x > 0 */
+		if (e2 < dx) { err += dx; y0 += sy; } /* e_xy+e_y < 0 */
+	}
+}
 
 PointcloudMapper2D::PointcloudMapper2D(std::string const& name)
     : PointcloudMapper2DBase(name)
@@ -27,8 +49,8 @@ bool PointcloudMapper2D::generate_map()
 	size_t arr_size = size_x * size_y;
 	int occ[arr_size];
 	int hit[arr_size];
-	memset(occ, 0, arr_size);
-	memset(hit, 0, arr_size);	
+	memset(occ, 0, arr_size * sizeof(int));
+	memset(hit, 0, arr_size * sizeof(int));	
 	
 	// Project pointclouds to temporary array
 	mLogger->message(INFO, "Requested traversability-grid generation.");
@@ -43,20 +65,28 @@ bool PointcloudMapper2D::generate_map()
 			mLogger->message(ERROR, "Measurement from PCL-Sensor is not a point cloud!");
 			return false;
 		}
-		PointCloud::ConstPtr cloud = pcl->getPointCloud();
-		for(PointCloud::const_iterator it = cloud->begin(); it < cloud->end(); it++)
+		
+		// Get sensor's pose in map coordinates
+		Transform sensorPose = (*it)->corrected_pose * pcl->getSensorPose();
+		
+		size_t sensorX, sensorY, pointX, pointY;
+		if(mGrid->toGrid(sensorPose.translation(), sensorX, sensorY))
 		{
-			size_t grid_x, grid_y;
-			if(mGrid->toGrid(Eigen::Vector3d(it->x, it->y, 0), grid_x, grid_y))
+			PointCloud::Ptr cloud(new PointCloud);
+			pcl::transformPointCloud(*(pcl->getPointCloud()), *cloud, sensorPose.matrix());
+			for(PointCloud::const_iterator it = cloud->begin(); it < cloud->end(); it++)
 			{
-				occ[(grid_y * size_x) + grid_x] += 1;
-				hit[(grid_y * size_x) + grid_x] += 1;
-				valid++;
+				if(mGrid->toGrid(Eigen::Vector3d(it->x, it->y, 0), pointX, pointY))
+				{
+					occ[(pointY * size_x) + pointX] += 1;
+					line(sensorX, sensorY, pointX, pointY, hit, size_x, size_y);
+					valid++;
+				}
+				count++;
 			}
-			count++;
 		}
 	}
-	std::cout << "Projected " << valid << " out of " << count << " points to the grid." << std::endl;
+	mLogger->message(DEBUG, (boost::format("Projected %1% out of %2% points to the grid.") % valid % count).str());
 	
 	// Write temporary array to traversability map
 	size_t index = 0;
@@ -65,17 +95,18 @@ bool PointcloudMapper2D::generate_map()
 		for(size_t x = 0; x < size_x; x++, index++)
 		{
 			int k = 0;
-			double p = 1;
 			if(hit[index] > 0)
 			{
 				if((occ[index] / hit[index]) > 0.5)
 				{
 					k = 1;
+				}else
+				{
+					k = 2;
 				}
 //				p = 2.0 * std::abs(0.5 - (occ[index] / hit[index]));
 			}
-//			std::cout << "Set (" << x << "," << y << ") to class '" << k << "' with p = " << p <<  std::endl;
-			mGrid->setTraversabilityAndProbability(k, p, x, y);
+			mGrid->setTraversabilityAndProbability(k, 1.0, x, y);
 		}
 	}
 	
@@ -104,8 +135,9 @@ bool PointcloudMapper2D::configureHook()
 	size_t x_size = mSizeX / mResolution;
 	size_t y_size = mSizeY / mResolution;
 	mGrid = new envire::TraversabilityGrid(x_size, y_size, mResolution, mResolution, mOffsetX, mOffsetY, "slam3d-grid");
-	mGrid->setTraversabilityClass(1, envire::TraversabilityClass(1.0));
-    mGrid->setTraversabilityClass(0, envire::TraversabilityClass(0.0));
+    mGrid->setTraversabilityClass(0, envire::TraversabilityClass(0.5));
+	mGrid->setTraversabilityClass(1, envire::TraversabilityClass(0.0));
+	mGrid->setTraversabilityClass(2, envire::TraversabilityClass(1.0));
 
 	// Add grid to environment
 	envire::FrameNode* grid_node = new envire::FrameNode();
